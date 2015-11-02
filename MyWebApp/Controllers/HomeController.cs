f100e3aa532ad54145eb4d2e9cb9eb79dd3c533d
Hwisco4u <hwisco4u@gmail.com>
2015-11-02 21:28:06 +0900

    홈화면수정

--- MyWebApp/Controllers/HomeController.cs
+++ MyWebApp/Controllers/HomeController.cs
@@ -8,6 +8,11 @@ namespace MyWebApp.Controllers
 {
     public class HomeController : Controller
     {
+        public class AboutDescription
+        {
+            public string Name { get; set; }
+            public string Description { get; set; }
+        }
         public ActionResult Index()
         {
             return View();
@@ -17,7 +22,7 @@ namespace MyWebApp.Controllers
         {
             ViewBag.Message = "Your application description page.";
 
-            return View();
+            return View(new AboutDescription { Name ="도원동교회", Description = "도원동교회 청년부입니다." });
         }
 
         public ActionResult Contact()
